732c9935e8d5800e238a44142b66389482671f6f
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2019-09-10 06:47:37 -0600

    working in iis, shutdown/iisreset triggers code shutdown

--- HostingShutdown3/Global.asax.cs
+++ HostingShutdown3/Global.asax.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Http;
 using System.Web.Routing;
+using log4net;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
 
 namespace HostingShutdown3
 {
@@ -13,6 +18,8 @@ namespace HostingShutdown3
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 
@@ -20,17 +27,20 @@ namespace HostingShutdown3
     {
         public static Guid InstanceId = Guid.NewGuid();
 
+        private static readonly ILog log = LogManager.GetLogger(typeof(SharedResource));
+
         public static void WriteToLog(Guid instanceId, string message)
         {
-            //using (var conn = new SqliteConnection("Filename=messages.db;"))
+            log.Info($"{DateTimeOffset.Now}, InstanceId={instanceId.ToString()}, Message={message}");
+            //using (var conn = new SQLiteConnection("Data Source=messages.db;New=true;"))
             //{
             //    conn.Open();
 
             //    using (var cmd = conn.CreateCommand())
             //    {
-            //        cmd.CommandText = "insert dbo.Messages (InstanceId,Message,Timestamp) values (@instanceId,@message,@timestamp)";
+            //        cmd.CommandText = "insert Messages (InstanceId,Content,Timestamp) values (@instanceId,@message,@timestamp)";
             //        cmd.Parameters.AddWithValue("@instanceId", instanceId);
-            //        cmd.Parameters.AddWithValue("@message", message);
+            //        cmd.Parameters.AddWithValue("@content", message);
             //        cmd.Parameters.AddWithValue("@timestamp", DateTimeOffset.Now);
 
             //        cmd.ExecuteNonQuery();
@@ -38,6 +48,29 @@ namespace HostingShutdown3
             //}
         }
     }
+
+    //public class AppContext : DbContext
+    //{
+    //    public DbSet<Message> Messages { get; set; }
+
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+
+    //    }
+
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+
+    //    }
+    //}
+
+    //public class Message
+    //{
+    //    public int Id { get; set; }
+    //    public string InstanceId { get; set; }
+    //    public string Content { get; set; }
+    //    public DateTime Timestamp { get; set; }
+    //}
 
     public class SharedResourceLifetime : IRegisteredObject
     {
@@ -60,13 +93,57 @@ namespace HostingShutdown3
 
     public sealed class SharedResource
     {
-        public static SharedResource Instance = new SharedResource();
+        public static SharedResource _instance = new SharedResource();
 
         private static SharedResourceLifetime Lifetime = new SharedResourceLifetime();
+
+        private IConnection _connection = null;
+
+        private static object _lock = new object();
+
+        public static SharedResource Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SharedResource();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
 
         private SharedResource()
         {
             Globals.WriteToLog(Globals.InstanceId, "SharedResource.ctor");
+
+            CreateConnection();
+        }
+
+        private void Shutdown()
+        {
+            _connection?.Dispose();
+
+            _instance = null;
+        }
+
+        private void CreateConnection()
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            // "guest"/"guest" by default, limited to localhost connections
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.VirtualHost = "/";
+            factory.HostName = "localhost";
+
+            _connection = factory.CreateConnection();
         }
     }
 }
